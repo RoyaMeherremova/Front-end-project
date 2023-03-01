48a17de29e3b6e3d7795da6bb53ec2ce2afddc9a
unknown <royaam@code.edu.az>
2023-03-02 00:01:39 +0400

    nav-area section

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,34 +1,37 @@
 $(document).ready(function () {
 
 
-    
-    let currency = $(".currency");
-
-    let usdBtn = $(".usd-btn");
-    let eurBtn = $(".eur-btn");
 
 
-    $(document).on("click", ".currency", function(){
+    $(document).on("click", ".currency", function () {
         $(".valyuta").toggleClass("d-none")
+        $(".language-area").addClass("d-none");
     })
 
-    $(document).on("click", ".usd-btn", function(){
-        let usdText =$(".usd-btn").children().eq(0).text();
+    $(document).on("click", ".usd-btn", function () {
+        let usdText = $(".usd-btn").children().eq(0).text();
         $(".currency").children().eq(0).text(usdText);
+        $(".valyuta").addClass("d-none");
+
     })
 
-    $(document).on("click", ".eur-btn", function(){
+    $(document).on("click", ".eur-btn", function () {
         let eurText = $(".eur-btn").children().eq(0).text();
         $(".currency").children().eq(0).text(eurText);
-     })
+           $(".valyuta").addClass("d-none")
+    })
+    $(document).on("mouseout", ".valyuta", function (){
+      
+    })
 
 
 
-     
+
     $(document).on("click", ".language", function (e) {
         e.preventDefault()
 
         $(".language-area").toggleClass("d-none");
+        $(".valyuta").addClass("d-none")
 
     });
     $(document).on("click", ".language-area button", function () {
@@ -37,6 +40,8 @@ $(document).ready(function () {
         $(".language-area").addClass("d-none");
 
     })
+  
+ 
 
 
 });
