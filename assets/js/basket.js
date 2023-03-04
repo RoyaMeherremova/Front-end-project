a8baa4c98cadaa0848cb97ba2b909305b55a762d
unknown <royaam@code.edu.az>
2023-03-04 14:57:52 +0400

    footer are ready

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
 
-
-      
 //responsiv navbar
 
 $(document).on("click", ".hamburger-menu",function(){
@@ -58,7 +56,7 @@ $(document).on("click",".sidebar-pages",function(e){
         $(".language-area").addClass("d-none");
 
     })
-  //basket icon 
+    //basket icon 
 
 
 
@@ -91,73 +89,11 @@ var swiper = new Swiper(".mySwiper", {
 
 
 
-  //tab-menu-slider
-
-
-   
-const tabLink = document.querySelectorAll(".tab-menu-link");
-const tabContent = document.querySelectorAll(".tab-bar-content");
-
-tabLink.forEach((item) => {
-    item.addEventListener("click", activeTab);
-});
-
-function activeTab(item) {
-    const btnTarget = item.currentTarget;
-    const content = btnTarget.dataset.content;
-
-    tabContent.forEach((item) => {
-        item.classList.remove("is-active");
-    });
-
-    tabLink.forEach((item) => {
-        item.classList.remove("is-active");
-    });
-
-    document.querySelector("#" + content).classList.add("is-active");
-    btnTarget.classList.add("is-active");
-}
 
 
 
 
 
-//tab-menu slider
-$('.cards').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
-});
 
 
-});
+})
