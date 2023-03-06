14db90ad93cc4a3707b9b5eb8fe2bf639fcf4eee
unknown <royaam@code.edu.az>
2023-03-06 13:44:35 +0400

    new section

--- assets/js/productDetail.js
+++ assets/js/productDetail.js
@@ -86,7 +86,29 @@ $(document).ready(function () {
 
     });
 
+// Tabs Action
+const tabLink = document.querySelectorAll(".tab-menu-link");
+const tabContent = document.querySelectorAll(".tab-bar-content");
 
+tabLink.forEach((item) => {
+  item.addEventListener("click", activeTab);
+});
+
+function activeTab(item) {
+  const btnTarget = item.currentTarget;
+  const content = btnTarget.dataset.content;
+
+  tabContent.forEach((item) => {
+    item.classList.remove("is-active");
+  });
+
+  tabLink.forEach((item) => {
+    item.classList.remove("is-active");
+  });
+
+  document.querySelector("#" + content).classList.add("is-active");
+  btnTarget.classList.add("is-active");
+}
 
 
 
